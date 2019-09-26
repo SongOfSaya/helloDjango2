2173ab0b0b311784e2aaefa09a34036c9562d175
songofsaya <liutengfei833@gmail.com>
2019-09-26 18:00:51 +0800

    step4

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 from django.http import Http404
-from .models import Question
+from .models import Question,Choice
 from django.template import loader
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {'latest_question_list': latest_question_list, }
     return render(request,'polls/index.html',context)
-
 
 def detail(request, question_id):
     try:
@@ -19,11 +18,14 @@ def detail(request, question_id):
         raise Http404("您要的问题不存在")
     return render(request, 'polls/detail.html',{'question':question})
 
-
 def results(request, question_id):
     response = "你正在查询问题 %s 的结果"
     return HttpResponse(response % question_id)
 
-
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except expression as identifier:
+        pass
     return HttpResponse("你正在为问题%s投票" % question_id)
