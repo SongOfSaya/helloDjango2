e0a336f25515dcb403d0ec4ac087d4b7973a24eb
刘腾飞 <liutengfei833@hotmail.com>
2019-09-27 01:16:23 +0800

    todo：第五部分

--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,61 @@
 from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
-from django.http import Http404
-from .models import Question,Choice
 from django.template import loader
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list, }
-    return render(request,'polls/index.html',context)
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+from .models import Question, Choice
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk = question_id)
-    except Question.DoesNotExist:
-        raise Http404("您要的问题不存在")
-    return render(request, 'polls/detail.html',{'question':question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = "你正在查询问题 %s 的结果"
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
+class DetailView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+ 
 def vote(request, question_id):
+    """函数说明"""
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except expression as identifier:
-        pass
-    return HttpResponse("你正在为问题%s投票" % question_id)
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message':"You 没有选择有效的选项"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+# 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("您要的问题不存在")
+#     return render(request, 'polls/detail.html', {'question':question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {
+#         'question':question
+#     })
+
+
